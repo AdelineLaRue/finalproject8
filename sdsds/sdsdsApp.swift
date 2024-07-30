61e4d2d206c756871427c25ee2325dfd05f7276f
hhh <hhh>
2024-07-30 09:37:27 -0400

    Initial Commit

---
+++ sdsds/sdsdsApp.swift
@@ -0,0 +1,17 @@
+//
+//  sdsdsApp.swift
+//  sdsds
+//
+//  Created by Scholar on 7/30/24.
+//
+
+import SwiftUI
+
+@main
+struct sdsdsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
